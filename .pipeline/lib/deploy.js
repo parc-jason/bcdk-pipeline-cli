43fe7b155b3c59f21727971d208392c0360b5c47
Jason Sherman <jsherman@parcsystems.ca>
2019-11-22 10:47:40 -0800

    test original examples

--- .pipeline/lib/deploy.js
+++ .pipeline/lib/deploy.js
@@ -1,27 +1,33 @@
-'use strict';
-const {OpenShiftClientX} = require('pipeline-cli')
-const path = require('path');
+"use strict";
+const { OpenShiftClientX } = require("pipeline-cli");
+const path = require("path");
 
-module.exports = (settings)=>{
-  const phases = settings.phases
-  const options= settings.options
-  const phase=options.env
-  const changeId = phases[phase].changeId
-  const oc=new OpenShiftClientX(Object.assign({'namespace':phases[phase].namespace}, options));
-  const templatesLocalBaseUrl =oc.toFileUrl(path.resolve(__dirname, '../../openshift'))
-  var objects = []
+module.exports = settings => {
+  const phases = settings.phases;
+  const options = settings.options;
+  const phase = options.env;
+  const oc = new OpenShiftClientX({ namespace: phases[phase].namespace });
+  const templatesLocalBaseUrl = oc.toFileUrl(path.resolve(__dirname, "../../openshift"));
+  var objects = [];
 
-  // The deployment of your cool app goes here ▼▼▼
-  objects = oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/python-deploy.yaml`, {
-    param: {
-      NAME: phases[phase].name,
-      SUFFIX: phases[phase].suffix,
-      VERSION: phases[phase].tag,
-      HOST: `${phases[phase].namespace}.pathfinder.gov.bc.ca`,
-    },
-  });
+  objects = objects.concat(
+    oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/python-deploy.yaml`, {
+      param: {
+        NAME: `${phases[phase].name}<%= name%>`,
+        SUFFIX: phases[phase].suffix,
+        VERSION: phases[phase].tag,
+        HOST: phases[phase].host || "",
+      },
+    }),
+  );
 
-  oc.applyRecommendedLabels(objects, phases[phase].name, phase, `${changeId}`, phases[phase].instance)
-  oc.importImageStreams(objects, phases[phase].tag, phases.build.namespace, phases.build.tag)
-  oc.applyAndDeploy(objects, phases[phase].instance)
-}
+  oc.applyRecommendedLabels(
+    objects,
+    phases[phase].name,
+    phase,
+    phases[phase].changeId,
+    phases[phase].instance,
+  );
+  oc.importImageStreams(objects, phases[phase].tag, phases.build.namespace, phases.build.tag);
+  oc.applyAndDeploy(objects, phases[phase].instance);
+};
