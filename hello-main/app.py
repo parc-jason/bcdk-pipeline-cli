71a1f27870ddd3211c6d74f8e7eb01709533ded9
Jason Sherman <jsherman@parcsystems.ca>
2019-11-22 12:03:22 -0800

    try some new templates

--- hello-main/app.py
+++ hello-main/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def dump_headers(path):
-  return 'REQUEST:\n{}\n\nHEADERS:\n{}\nHOSTNAME:{}'.format(request.method + ' ' + request.url, str(request.headers), platform.node()), 200, {'Content-Type': 'text/plain; charset=utf-8'}
+  return '{}\n\nHEADERS:\n{}\nHOSTNAME:{}'.format(request.method + ' ' + request.url, str(request.headers), platform.node()), 200, {'Content-Type': 'text/plain; charset=utf-8'}
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
