ea2c016c28af8bd615034c1f124955752142404e
Jason Sherman <jsherman@parcsystems.ca>
2019-11-22 11:03:45 -0800

    guess mine were good...

--- .pipeline/lib/config.js
+++ .pipeline/lib/config.js
@@ -1,50 +1,20 @@
-"use strict";
-const options = require("pipeline-cli").Util.parseArguments();
-const changeId = options.pr; //aka pull-request
-const version = "1";
-const name = "hello";
+'use strict';
+const options= require('pipeline-cli').Util.parseArguments()
+const changeId = options.pr //aka pull-request
+const version = '1.0.0'
+const name = 'hello'
 
 const phases = {
-  build: {
-    namespace: "wfezkf-tools",
-    name: `${name}`,
-    phase: "build",
-    changeId: changeId,
-    suffix: `-build-${changeId}`,
-    instance: `${name}-build-${changeId}`,
-    version: `${version}-${changeId}`,
-    tag: `build-${version}-${changeId}`,
-  },
-  dev: {
-    namespace: `wfezkf-dev`,
-    name: `${name}`,
-    phase: "dev",
-    changeId: changeId,
-    suffix: `-dev-${changeId}`,
-    instance: `${name}-dev-${changeId}`,
-    version: `${version}-${changeId}`,
-    tag: `dev-${version}-${changeId}`,
-  },
-  test: {
-    namespace: `wfezkf-test`,
-    name: `${name}`,
-    phase: "test",
-    changeId: changeId,
-    suffix: `-test-${changeId}`,
-    instance: `${name}-test-${changeId}`,
-    version: `${version}-${changeId}`,
-    tag: `test-${version}`,
-  },
-  prod: {
-    namespace: `wfezkf-prod`,
-    name: `${name}`,
-    phase: "prod",
-    changeId: changeId,
-    suffix: "",
-    instance: `${name}-prod`,
-    version: `${version}-${changeId}`,
-    tag: `prod-${version}`,
-  },
+  build: {namespace:'wfezkf-tools'    , name: `${name}`, phase: 'build'  , changeId:changeId, suffix: `-build-${changeId}`  , instance: `${name}-build-${changeId}`  , version:`${version}-${changeId}`, tag:`build-${version}-${changeId}`},
+  dev: {namespace:'wfezkf-dev'    , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-dev-${changeId}`  , instance: `${name}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`dev-${version}-${changeId}`},
+  test: {namespace:'wfezkf-test'    , name: `${name}`, phase: 'test'  , changeId:changeId, suffix: `-test`  , instance: `${name}-test`  , version:`${version}`, tag:`test-${version}`},
+  prod: {namespace:'wfezkf-prod'    , name: `${name}`, phase: 'prod'  , changeId:changeId, suffix: `-prod`  , instance: `${name}-prod`  , version:`${version}`, tag:`prod-${version}`},
 };
+
+// This callback forces the node process to exit as failure.
+process.on('unhandledRejection', (reason) => {
+  console.log(reason);
+  process.exit(1);
+});
 
 module.exports = exports = {phases, options};
--- .pipeline/lib/deploy.js
+++ .pipeline/lib/deploy.js
@@ -1,33 +1,27 @@
-"use strict";
-const { OpenShiftClientX } = require("pipeline-cli");
-const path = require("path");
+'use strict';
+const {OpenShiftClientX} = require('pipeline-cli')
+const path = require('path');
 
 module.exports = (settings)=>{
-  const phases = settings.phases;
-  const options = settings.options;
-  const phase = options.env;
-  const oc = new OpenShiftClientX(Object.assign({'namespace':phases.build.namespace}, options));
-  const templatesLocalBaseUrl = oc.toFileUrl(path.resolve(__dirname, "../../openshift"));
-  var objects = [];
+  const phases = settings.phases
+  const options= settings.options
+  const phase=options.env
+  const changeId = phases[phase].changeId
+  const oc=new OpenShiftClientX(Object.assign({'namespace':phases[phase].namespace}, options));
+  const templatesLocalBaseUrl =oc.toFileUrl(path.resolve(__dirname, '../../openshift'))
+  var objects = []
 
-  objects = objects.concat(
-    oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/python-deploy.yaml`, {
-      param: {
-        NAME: `${phases[phase].name}<%= name%>`,
-        SUFFIX: phases[phase].suffix,
-        VERSION: phases[phase].tag,
-        HOST: phases[phase].host || "",
-      },
-    }),
-  );
+  // The deployment of your cool app goes here ▼▼▼
+  objects = oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/python-deploy.yaml`, {
+    param: {
+      NAME: phases[phase].name,
+      SUFFIX: phases[phase].suffix,
+      VERSION: phases[phase].tag,
+      HOST: `${phases[phase].namespace}.pathfinder.gov.bc.ca`,
+    },
+  });
 
-  oc.applyRecommendedLabels(
-    objects,
-    phases[phase].name,
-    phase,
-    phases[phase].changeId,
-    phases[phase].instance,
-  );
-  oc.importImageStreams(objects, phases[phase].tag, phases.build.namespace, phases.build.tag);
-  oc.applyAndDeploy(objects, phases[phase].instance);
-};
+  oc.applyRecommendedLabels(objects, phases[phase].name, phase, `${changeId}`, phases[phase].instance)
+  oc.importImageStreams(objects, phases[phase].tag, phases.build.namespace, phases.build.tag)
+  oc.applyAndDeploy(objects, phases[phase].instance)
+}
